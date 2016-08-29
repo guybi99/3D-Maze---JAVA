0f1d9354fdfb9e39f4c2f72539a486bfca20c773
TalM <docbelton1@gmail.com>
2016-08-29 17:02:15 +0300

    started project part 3

---
+++ My_Algorithms/src/io/MyCompressorOutputStream.java
@@ -0,0 +1,19 @@
+package io;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class MyCompressorOutputStream extends OutputStream {
+
+	private OutputStream out;
+	
+	public MyCompressorOutputStream(OutputStream out) {
+		super();
+		this.out = out;
+	}
+
+	@Override
+	public void write(int b) throws IOException {
+		// TODO Auto-generated method stub
+	}
+}
---
+++ My_Algorithms/src/io/MyDecompressorInputStream.java
@@ -0,0 +1,21 @@
+package io;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class MyDecompressorInputStream extends InputStream{
+	
+	InputStream in;
+	
+	public MyDecompressorInputStream(InputStream in) {
+		super();
+		this.in = in;
+	}
+
+	@Override
+	public int read() throws IOException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
