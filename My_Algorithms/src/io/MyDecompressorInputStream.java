4182b6e8cb9ec4fde2c678e02f9c1af90cf6af33
TalM <docbelton1@gmail.com>
2016-08-31 17:03:54 +0300

    finished decompressor

--- My_Algorithms/src/io/MyDecompressorInputStream.java
+++ My_Algorithms/src/io/MyDecompressorInputStream.java
@@ -12,10 +12,44 @@ public class MyDecompressorInputStream extends InputStream{
 		this.in = in;
 	}
 
+	/**
+	 * this method is read from the file array of bytes and bring us what he read
+	 * @param b is the array we want to read
+	 * @return int
+	 */
 	@Override
-	public int read() throws IOException {
-		// TODO Auto-generated method stub
-		return 0;
+	public int read(byte[] b) throws IOException
+	{
+		byte by;
+		int temp;
+		int count = 0;
+		int i = 0;
+		
+		while( (temp = in.read()) != -1) {
+			by =  new Integer(temp).byteValue();
+			count = in.read();
+//			System.out.println(by);
+//			System.out.println(count);
+			while(count>0)
+			{
+				b[i] = by;
+				count--;
+				i++;
+			}
+		}
+		
+		return 1;
+	}
+	
+	/**
+	 * this method is read from the file and bring us what he get
+	 * @return int
+	 */
+	@Override
+	public int read() throws IOException 
+	{		
+		return (int)in.read();
+		
 	}
 
 }
