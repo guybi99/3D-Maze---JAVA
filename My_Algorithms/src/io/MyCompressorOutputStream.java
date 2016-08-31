dfcaaf62cf97103430f889e638a5a875018fcb2c
TalM <docbelton1@gmail.com>
2016-08-31 17:04:37 +0300

    finished writes methods

--- My_Algorithms/src/io/MyCompressorOutputStream.java
+++ My_Algorithms/src/io/MyCompressorOutputStream.java
@@ -11,9 +11,55 @@ public class MyCompressorOutputStream extends OutputStream {
 		super();
 		this.out = out;
 	}
+	
+	@Override
+	public void write(byte[] byte_array){
+		int counter = 0;
+		byte last = 0;
+		
+		for (int i = 0; i < byte_array.length; i++){
+			if(byte_array[i] != last){
+				
+				try {
+					if(counter > 0){
+						out.write(last);
+						out.write(counter);
+					}
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+				
+				last = byte_array[i];
+				counter = 1;
+				
+			}else{
+				counter++;
+				
+				if(counter == 255){
+					try {
+						out.write(last);
+						out.write(counter);
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				
+					counter = 0;
+				}
+			}
+		}
+		
+		if(counter > 0){
+			try {
+				out.write(last);
+				out.write(counter);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
 
 	@Override
 	public void write(int b) throws IOException {
-		// TODO Auto-generated method stub
+		out.write((byte) b);
 	}
 }
