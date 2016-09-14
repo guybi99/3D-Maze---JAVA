9261364a1a6e83428f36ee647584f39b381ed1fc
TalM <docbelton1@gmail.com>
2016-09-14 21:41:53 +0300

    imports

--- MVC/src/boot/Run.java
+++ MVC/src/boot/Run.java
@@ -7,42 +7,34 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.util.HashMap;
 
-import algorithms.mazeGenarators.Maze3d;
-import algorithms.mazeGenarators.Position;
-import algorithms.mazeGenarators.SimpleMaze3dGenerator;
-import algorithms.search.BFS;
-import algorithms.demo.SearchableAdapter;
-import algorithms.search.Solution;
 import model.MyModel;
 import view.MyView;
-import view.CLI;
 import controller.*;
 
 public class Run {
 
 	public static void main(String[] args) throws IOException
 	{
-		new File("MyMaze");
-		
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 		PrintWriter out = new PrintWriter(System.out);
 		
 		MyModel mm = new MyModel();
 		HashMap<String,Command> hm = new HashMap<String,Command>();
 		
-		hm.put("Dir", new Dir(mm));
-		hm.put("Generate3DMaze", new Generate3DMaze(mm));
-		hm.put("Display", new Display(mm));
-		hm.put("DisplayCrossSectionBy", new DisplayCrossSectionBy(mm));
-		hm.put("SaveMaze", new SaveMaze(mm));
-		hm.put("LoadMaze", new LoadMaze(mm));
-		hm.put("Solve", new Solve(mm));
-		hm.put("DisplaySolution", new DisplaySolution(mm));
-		hm.put("Exit", new Exit(mm));
+		hm.put("dir", new Dir(mm));
+		hm.put("generate_3d_maze", new Generate3DMaze(mm));
+		hm.put("display", new Display(mm));
+		hm.put("display_cross_section", new DisplayCrossSectionBy(mm));
+		hm.put("save_maze", new SaveMaze(mm));
+		hm.put("load_maze", new LoadMaze(mm));
+		hm.put("solve", new Solve(mm));
+		hm.put("display_solution", new DisplaySolution(mm));
+		hm.put("exit", new Exit(mm));
 		
 		MyView mv = new MyView(in, out, hm);
 		
 		MyController c = new MyController(mv, mm, hm);
+		mm.setC(c);
 		mv.setController(c);
 		
 		c.getMv().start();
